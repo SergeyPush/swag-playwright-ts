67f305b7bd6cd8c3896b6bcef0b05708b3ace9e4
Sergey <spushkovskiy@gmail.com>
2023-07-31 11:20:19 +0300

    Changed browser for tests

--- playwright.config.ts
+++ playwright.config.ts
@@ -41,16 +41,19 @@ export default defineConfig({
   projects: [
     {
       name: "chromium",
-      use: { ...devices["Desktop Chrome"] },
+      use: {
+        // ...devices["Desktop Chrome"],
+        channel: "chrome",
+      },
     },
-    {
-      name: "firefox",
-      use: { ...devices["Desktop Firefox"] },
-    },
-    {
-      name: "webkit",
-      use: { ...devices["Desktop Safari"] },
-    },
+    // {
+    //   name: "firefox",
+    //   use: { ...devices["Desktop Firefox"] },
+    // },
+    // {
+    //   name: "webkit",
+    //   use: { ...devices["Desktop Safari"] },
+    // },
 
     /* Test against mobile viewports. */
     // {
