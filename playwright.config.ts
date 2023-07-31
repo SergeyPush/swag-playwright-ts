d008a0a9f65dd4d3005aa5a596eb28530c4f2502
Sergey <spushkovskiy@gmail.com>
2023-07-31 21:21:10 +0300

    Updated reporter

--- playwright.config.ts
+++ playwright.config.ts
@@ -21,9 +21,10 @@ export default defineConfig({
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   // reporter: "list",
-  reporter: "html",
+  // reporter: "html",
   // reporter: "dot",
   // reporter: "line",
+  reporter: [["list"], ["junit", { outputFile: "test-results/report.xml" }]],
 
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
